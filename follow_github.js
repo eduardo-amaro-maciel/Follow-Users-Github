6d88339ea453e94ad099f0f7dccc64a15437639b
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2022-06-24 16:06:31 -0300

    Add files via upload

---
+++ follow_github.js
@@ -0,0 +1,19 @@
+function expectPromise() {
+    return new Promise((resolve, reject) => {
+        let token = document.querySelectorAll('div[class="d-table-cell col-2 v-align-top text-right"] span form input[type="hidden"]')
+
+        Array.from(token).forEach(e => {
+            let button = document.querySelector(`input[value="${e.value}"] ~ input[type="submit"]`)
+            if(button.value == 'Follow') {
+                button.click()
+            }
+        })
+        resolve(true)
+    })
+}
+
+expectPromise().then(e => {
+    if(e) {
+        document.querySelector('.paginate-container .pagination ').children[1].click()
+    }
+})
